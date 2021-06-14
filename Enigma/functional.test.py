c968f0108916cb1639df13ac116b8c863f164812
Andrew Twydell <am.twydell@gmail.com>
2021-06-14 12:29:47 +0100

    working test to run
    Signed-off-by: Andrew Twydell <am.twydell@gmail.com>

---
+++ Enigma/functional.test.py
@@ -0,0 +1,10 @@
+import enigma
+import argparse
+import re
+
+machine = enigma.createEnigma(["I", "II", "III"], [0, 15, 3], [3, 25, 12], "B", "AB CD EF GH IJ KL MN OP")
+
+plaintext = "RYXSTXPKLBIGBKZFKIDSRVF"
+ciphertext = machine.encrypt(plaintext)
+
+print(ciphertext)
