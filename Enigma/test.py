d7f6af657eb52bd7d02455cc0fd4496dbd32bd69
Jack Burne <jack_burne@yahoo.co.uk>
2021-06-19 17:58:09 +0100

    adding capability to read and write to files

--- Enigma/test.py
+++ Enigma/test.py
@@ -3,6 +3,9 @@ import argparse
 import re
 
 def main():
+    # escaping our regex to check for valid file paths
+    validFilePath = re.compile(r'^(?:[a-z]:)?[\/\\]{0,2}(?:[.\/\\ ](?![.\/\\\n])|[^<>:\"|?*!.\/\\ \n])+$')
+
     # Creating the argument parser to handle the user inputting their settings
     parser = argparse.ArgumentParser(description="Encrypts a text file using an implementation of the Enigma Machine")
 
@@ -15,15 +18,9 @@ def main():
 
     # Positional Arguments
     parser.add_argument("-rf", "--readfile", help="Reads in Cipher Text from a specified file", type=str)
-    parser.add_argument("-wf", "--writefile", help="Writes the output of the machine to a specified file", type=str)
+    parser.add_argument("-wf", "--writefile", help="Writes the output of the machine to a specified file, or to ciphertext.txt", type=str)
     # Parsing Arguments into the program
     args = parser.parse_args()
-
-    print("Chosen Rotors: " + args.Rotors)
-    print("Initial Rotor Positions: " + str(splitChars(args.rPos)))
-    print("Ring Settings: " + str(splitChars(args.rSet)))
-    print("Chosen Reflector: " + args.Reflector)
-    print("Plugboard Wiring: " + args.Plugboard)
 
     # Creating an instance of the enigma machine, with our given settings, and doing any formatting as necerssary
     enig = enigma.Enigma(re.split("[^a-zA-Z]", args.Rotors),
@@ -32,14 +29,64 @@ def main():
                   args.Reflector,
                   args.Plugboard)
 
-    print("encrypting hard coded text...")
-    print(enig.encrypt("AAAAA"))
-    print("Finished Encryption...")
-    a = enig.encrypt("A Aa Aa.#")
-    print(a)
-    print(enig.encrypt(a))
-    print("Finished Tests...")
+
+    # Checking if the User wanted to read in the plain text from a file
+    if args.readfile != None:
+        # If they do, we check we have been given a valid file path
+        if validateFileName(args.readfile, validFilePath):
+            # If we are, we read in the values from the text
+            pText = getpText(args.readfile)
+
+    # Running our plain text through the machine 
+    cText = enig.encrypt(pText)
+
+    # Checking if the User wanted to write the output text to a file
+    if args.writefile != None:
+        # Validating the output filepath
+        if validateFileName(args.writefile, validFilePath):
+            # Trying to write the file
+            if writecText(cText, args.writefile):
+                print("Written cipher text to: " + str(args.writefile))
     
+
+# Function for getting the plain text from a text file
+def getpText(pTextPath):
+    # Try to open the file path given
+    try:
+        # Open the file and read the contents
+        pText = open(pTextPath, "r")
+        pTextContents = pText.read()
+    except:
+        print("Error Opening File...")
+    finally:
+        pText.close()
+    
+    return pTextContents
+
+
+# Function for writing the cipher text to a file
+def writecText(cText, cTextPath):
+    try:
+        # Opening a python file to write too
+        cTextFile = open(cTextPath, "w")
+        # Writing our cipher text
+        cTextFile.write(cText)
+    except:
+        print("Error writing to an output file")
+    finally:
+        cTextFile.close()
+
+    return True
+
+# Function for validating that the User input file is a valid file path
+def validateFileName(pathToCheck, validFilePath):
+    # If our string is a valid file path, we return True, else False and print to the terminal a message
+    if re.search(validFilePath, pathToCheck):
+        return True
+    else:
+        print("Not a Valid File Path!")
+        return False
+
 
 # Small function for handling splitting up a string into an array, based on spaces, and then
 # converting it to an ASCII value, and subbing 65, to create a value between 0 - 25
@@ -56,9 +103,3 @@ def splitChars(rPos):
 
 
 main()
-
-# machine = enigma.Enigma(["I", "II", "III"], [11, 13, 15], [19, 20, 21], "B", "AB CD EF GH IJ KL MN OP")
-
-
-# pText = "ANDREWISTWAT"
-# print(machine.encrypt(pText))
